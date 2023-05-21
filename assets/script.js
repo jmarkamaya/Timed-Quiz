6d19ba539e90fc26c8fc9b0f43ba4682f4a88357
Jon Amaya <jonamaya@Jons-MacBook-Air.local>
2023-05-21 00:09:19 -0700

    added start game function, used javascript append to make questions appear

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,71 @@
-var startButton = document.querySelector(".start")
-var body = document.body
+var startButton = document.querySelector(".start");
+var body = document.body;
+var questionEl = document.querySelector(".question");
+var answerEl = document.querySelector(".answer");
+var listEl = document.querySelector('.choices')
 var questions = [
-    {
-      title: 'Commonly used data types DO NOT include:',
-      choices: ['strings', 'booleans', 'alerts', 'numbers'],
-      answer: 'alerts',
-    },
-    {
-      title: 'The condition in an if / else statement is enclosed within ____.',
-      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-      answer: 'parentheses',
-    },
-    {
-      title: 'Arrays in JavaScript can be used to store ____.',
-      choices: [
-        'numbers and strings',
-        'other arrays',
-        'booleans',
-        'all of the above',
-      ],
-      answer: 'all of the above',
-    },
-    {
-      title:
-        'String values must be enclosed within ____ when being assigned to variables.',
-      choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-      answer: 'quotes',
-    },
-    {
-      title:
-        'A very useful tool used during development and debugging for printing content to the debugger is:',
-      choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
-      answer: 'console.log',
-    },
-  ];
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers',],
+    answer: 'alerts',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+];
+var currentQuestion = questions[0]
+
+
 
 function startQuiz() {
-   var questionTemplate = document.createElement("div")
 
-   questionTemplate.textContent = "Testing 1, 2, 3";
+  questionEl.textContent = currentQuestion.title;
 
-   body.appendChild(questionTemplate);
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
 
+    var choiceOption = currentQuestion.choices[i];
+
+    var li = document.createElement("li");
+    li.setAttribute("style", "list-style: none")
+    li.textContent = choiceOption;
+    var inputEl = document.createElement("input");
+    li.appendChild(inputEl);
+    inputEl.setAttribute("type", "radio");
+    inputEl.setAttribute("name", "option");
+    inputEl.setAttribute("value", choiceOption);
+    
+    listEl.appendChild(li);
+
+  };
 
 };
 
 startButton.addEventListener("click", function () {
-    startQuiz()
+  this.setAttribute("style", "display: none")
+  startQuiz()
 });
+
